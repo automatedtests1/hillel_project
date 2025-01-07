ad5d35fffe21eb01dc51a88f2e5df52328a7d93b
automatedtests1 <krolevetska.kateryna@gmail.com>
2025-01-07 21:19:56 +0200

    thirteen

--- Square.java
+++
@@ -1,9 +0,0 @@
-public class Square {
-    public static void printSquare(int number) {
-        System.out.println("Квадрат числа " + number + " дорівнює " + (number * number));
-
-public class calculateCylinderVolume
- {public static double calculateCylinderVolume(double radius, double height) {
-    return Math.PI * radius * radius * height;}
-
-    
--- lesson8/CounterHIIndexOf.java
+++ lesson8/CounterHIIndexOf.java
@@ -11,13 +11,13 @@ public class CounterHIIndexOf {
     }
 
     public static void main(String[] args) {
-        // Приклади використання (аналогічні попередньому прикладу)
+        // Приклади використання
          System.out.println("countHi(\"abc hi ho\") -> " + countHi("abc hi ho")); // Виведе 1
         System.out.println("countHi(\"ABChi hi\") -> " + countHi("ABChi hi")); // Виведе 2
-        System.out.println("countHi(\"hihi\") -> " + countHi("hihi")); // Виведе 2
+        System.out.println("countHi(\"HIFI\") -> " + countHi("hifi")); // Виведе 2
         System.out.println("countHi(\"h\") -> " + countHi("h")); // Виведе 0
         System.out.println("countHi(\"\") -> " + countHi("")); // Виведе 0
         System.out.println("countHi(\"hi\") -> " + countHi("hi")); // Виведе 1
-         System.out.println("countHi(\"xhixhix\") -> " + countHi("xhixhix")); // Виведе 2
+         System.out.println("countHi(\"xxix\") -> " + countHi("xxix")); // Виведе 2
     }
 }
